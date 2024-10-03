00b878828ea561053ece90cb5ee6088c1b6d73ed
Madiba Hudson-Quansah <mhquansah@gmail.com>
2024-10-03 19:40:26 +0000

    Added overwrite flag

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import signal
 from typing import Iterable
 from argparse import ArgumentParser
-
 from db_utils import createDatabase
 from search_load import searchFile, loadFile, loadFolders
 
@@ -42,11 +41,18 @@ if __name__ == "__main__":
         help="Verbose output",
     )
 
-    args = parser.parse_args()
+    parser.add_argument(
+        "--overwrite",
+        default=False,
+        action="store_true",
+        help="Overwrite existing db",
+    )
 
+    args = parser.parse_args()
     mode = args.mode
     filename = args.filename
     v = args.verbose
+    overwrite = args.overwrite
 
     db = "dbname=tones user=mads"
 
@@ -56,6 +62,9 @@ if __name__ == "__main__":
         case "load":
             loadFile(db, filename, verbose=v)
         case "load_folder":
+            if overwrite:
+                print("Overwriting database")
+                createDatabase(db, "./src/db/schema.sql")
             loadFolders(db, Path(filename), verbose=v, maxWorkers=5)
         case "search":
             res = searchFile(db, filename, verbose=v)
